523fcc52e4e9d76d6434bf936660e7a24d13c5fa
Deepak Agnihotri <deepak.agnihotri@9lenses.com>
2015-08-27 15:17:51 +0530

    Initial Commit

---
+++ Crossover.Web.Sample1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Crossover.Web.Sample1.MvcApplication" Language="C#" %>
---
+++ Crossover.Web.Sample1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Crossover.Web.Sample1.Startup))]
+namespace Crossover.Web.Sample1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
