c53a14658426909dfd83ebce9df14e46fdb74efd
Deepak Agnihotri <deepak.agnihotri@9lenses.com>
2015-08-29 18:32:01 +0530

    Created About View

--- Crossover.Web.Sample2/Controllers/HomeController.cs
+++ Crossover.Web.Sample2/Controllers/HomeController.cs
@@ -13,13 +13,24 @@ namespace Crossover.Web.Sample2.Controllers
     /// </summary>
     public class HomeController : Controller
     {
-        [Authorize]
         /// <summary>
         /// Renders the home page of the user.
         /// </summary>
         /// <returns>The home page view.</returns>
+        [Authorize]
         public ActionResult Index()
         {
+            return View();
+        }
+
+        /// <summary>
+        /// Renders the about page of the company.
+        /// </summary>
+        /// <returns>The about page view.</returns>
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
             return View();
         }
     }
